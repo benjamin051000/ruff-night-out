cdd4ae32f7474db97fda9638ce35508d808d771e
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-08-28 17:54:37 -0400

    updated door sprite w/ shadow
    Signed-off-by: Benjamin Wheeler <benjaminwheeler0510@gmail.com>

--- src/world/door.gd
+++ src/world/door.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-@onready var sprite_2d: Sprite2D = $Mask/Sprite2D
+@onready var sprite_2d: Sprite2D = $Door
+@onready var shadow: Sprite2D = $Door/Shadow
 
 signal door_opened
 signal door_closed
@@ -24,9 +25,10 @@ func _ready() -> void:
 	close_x = sprite_2d.position.x
 	open_x = sprite_2d.position.x - sprite_2d.texture.get_size().x
 
-func _pos_helper(pos: float, sig: Signal) -> void:
+func _pos_helper(pos: float, shadow_off: float, sig: Signal) -> void:
 	var pos_tween = create_tween()
 	pos_tween.tween_property(sprite_2d, "position:x", pos, t).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
+	pos_tween.parallel().tween_property(shadow, "position:x", shadow_off, t).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
 	pos_tween.tween_callback(func(): sig.emit())
 
 func _lp_helper(cutoff: float) -> void:
@@ -34,11 +36,11 @@ func _lp_helper(cutoff: float) -> void:
 	lp_tween.tween_property(music_lp_filter, "cutoff_hz", cutoff, t).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUART)
 
 func open() -> void:
-	_pos_helper(open_x, door_opened)
+	_pos_helper(open_x, close_x - open_x, door_opened)
 	_lp_helper(20500)  # Max allowed
 	$OpenSound.play()
 
 func close() -> void:
-	_pos_helper(close_x, door_closed)
+	_pos_helper(close_x, 0, door_closed)
 	_lp_helper(lp_cutoff)
 	$CloseSound.play()
