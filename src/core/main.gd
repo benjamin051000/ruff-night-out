85ef5c0ab1bd644d49373146beecb4fd32bd9a65
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-08-28 15:21:18 -0400

    remove unused _process()
    Signed-off-by: Benjamin Wheeler <benjaminwheeler0510@gmail.com>

--- src/core/main.gd
+++ src/core/main.gd
@@ -33,9 +33,6 @@ func _ready() -> void:
 	)
 	door.open()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_button_pressed() -> void:
