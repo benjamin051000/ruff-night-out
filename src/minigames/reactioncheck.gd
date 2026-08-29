046294817a740cdbc9984f88a692b0947d29158a
bit-bright <brightnightlight@hotmail.com>
2026-08-29 15:54:36 -0400

    added code for guest responses and whiteboard placeholder code

--- src/minigames/reactioncheck.gd
+++ src/minigames/reactioncheck.gd
@@ -9,6 +9,7 @@ var cat = preload("res://assets/minigame/cat.png")
 var tennisball = preload("res://assets/minigame/tennisball.png")
 var treat = preload("res://assets/minigame/treat.png")
 
+
 var guestresponse 
 var Guest 
 var dogtype
@@ -30,8 +31,41 @@ func get_random_items(count:int) -> Array:
 	keys.shuffle()
 	return keys.slice(0, count)
 
-# TO DO : - need to hold these and display corresponding images on bouncer whiteboard
+const bouncertext := [
+	"Hey, do you mind taking a look at this?",
+	"My thoughts exactly.",
+	"Uh huh, very interesting. Take a walk!"
+]
+# bouncertext[1] = always at start of minigame
+# bouncertext[2] = if accept button is pressed
+# bouncertext[3] = if reject button is pressed
 
+# speechbubble call logic? May need changed/signals adjusted
+# var i := 0
+#
+#func _unhandled_input(event: InputEvent) -> void:
+	#if event is InputEventMouseButton and event.is_pressed():
+		#if i >= text.size():
+			#get_tree().quit()  # Not instant, so return too
+			#return
+#
+		#$SpeechBubble.display(text[i])
+		#i += 1
+		
+const bouncer_board = preload("res://assets/px_bouncer_armraise.png")
+
+# function to display corresponding images on bouncer whiteboard
+func assign_to_whiteboard(items:Array):
+	$Whiteboard/TextureRect.texture = items[0]
+	$Whiteboard/TextureRect2.texture = items[1]
+	$Whiteboard/TextureRect3.texture = items[2]
+	# bouncer sprite -> bouncer_board + whiteboard added to scene
+
+	# trigger/timer that changes to textrect1/2/3 
+	# these display ON TOP of whiteboard 
+	# with wait time between each
+	
+	# whiteboard no longer shown, bouncer spirte lowers arms
 
 func get_guest_responses(items:Array) -> Array:
 	var result = []
@@ -46,14 +80,48 @@ func get_guest_responses(items:Array) -> Array:
 
 	return result
 
+var like_response = preload("res://assets/px_speechbubble_other.png")
+var dislike_response = preload("res://assets/px_speechbubble_other.png")
+
+var actual_responses = {
+	like_response : 0,
+	dislike_response : 1 
+}
+
+func display_guest_resp(result: Array) -> void:
+	var rects = [
+		$GuestResp/Response1,
+		$GuestResp/Response2,
+		$GuestResp/Response3
+	]
+	for r in rects:
+		r.visible = false
+
+	for i in range(result.size()):
+		var value = result[i]
+		# Find which image matches value
+		var img = null
+		for key in actual_responses.keys():
+			if actual_responses[key] == value:
+				img = key
+				break
+		# Assign the correct speech bubble image
+		rects[i].texture = img
+		rects[i].visible = true
+		await get_tree().create_timer(0.5).timeout
+
 #To Do: - make response sprites
-# - need to associate responses to a 
-# - second dictionary with response images for guest
-# - which also activates speech bubble above head
+# - which is shown in a speech bubble above head
 
 func _ready() -> void:
 	Guest = preload("res://src/characters/guest.tscn").instantiate()
 	dogtype = Guest.dogtype
 	
 	var items = get_random_items(3)
+	
+	# bouncertext[1] display in bubble
+	assign_to_whiteboard(items)
 	guestresponse = get_guest_responses(items)
+	#display guest response in speech bubble
+	
+	#end minigame and clear queue
