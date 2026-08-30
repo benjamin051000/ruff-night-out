60e56af92503f6f33e0d32870b92db616b5ea993
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2026-08-30 01:32:05 -0400

    remove unused boss funcs
    Signed-off-by: Benjamin Wheeler <benjaminwheeler0510@gmail.com>

--- src/characters/boss.gd
+++ src/characters/boss.gd
@@ -1,16 +1,5 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func exit_door() -> void:
 	var old_z_index := z_index
 	var old_scale := scale
